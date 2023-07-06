5bb1dbbaa8d83666ac1ff0063f220ae77a4e5b64
Nitin kumar <nimble2905@gmail.com>
2023-07-06 23:41:49 +0530

    adding routes component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import { useState } from "react";
+import Footer from "./Components/Footer";
+import ProjectSection from "./Components/Routes/ProjectSection";
+import ServicesSection from "./Components/Routes/ServicesSection";
+import Contact from "./Components/Routes/ContactSection";
+import Connect from "./Components/Routes/Connect";
+import Login from "./Components/Authentication/Login";
+import SignUp from './Components/Authentication/SignUp'
 
 function App() {
   const [mode, setmode] = useState("Dark");
@@ -13,17 +20,23 @@ function App() {
       setmode("Dark");
     }
   };
-  document.body.style.backgroundColor = `${
-    mode === "Dark" ? "rgb(17 24 39)" : "#D6D5DB"
-  }`;
+  document.body.style.backgroundColor = `${mode === "Dark" ? "rgb(17 24 39)" : "#D6D5DB"
+    }`;
   return (
-    <div className="m-3 position-relative">
+    <div className="mx-3 position-relative">
       <Router>
         <Navbar mode={mode} toggleMode={toggleMode} />
-        <Home mode={mode} toggleMode={toggleMode} />
         <Routes>
-          {/* <Route exact index element={<Home mode={mode} />} /> */}
+          <Route exact index element={<Home mode={mode} />} />
+          <Route exact path="/" element={<Home mode={mode} toggleMode={toggleMode} />}></Route>
+          <Route exact path="/projects" element={<ProjectSection mode={mode} toggleMode={toggleMode} />}></Route>
+          <Route exact path="/services" element={<ServicesSection mode={mode} toggleMode={toggleMode} />}></Route>
+          <Route exact path="/contact" element={<Contact mode={mode} toggleMode={toggleMode} />}></Route>
+          <Route exact path="/connect" element={<Connect mode={mode} toggleMode={toggleMode} />}></Route>
+          <Route exact path="/login" element={<Login mode={mode} toggleMode={toggleMode} />}></Route>
+          <Route exact path="/signUp" element={<SignUp mode={mode} toggleMode={toggleMode} />}></Route>
         </Routes>
+        <Footer mode={mode} />
       </Router>
     </div>
   );

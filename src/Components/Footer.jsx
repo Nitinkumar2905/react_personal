968d4007f63dc6ae0ee625d8b6dae59ffafc797a
Nitin kumar <nimble2905@gmail.com>
2023-07-10 22:25:13 +0530

    added link in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,14 +3,23 @@ import "./styles/Footer.css";
 const Footer = (props) => {
   return (
     <>
-      <footer
-        className={`text-center`}
-        style={{ fontFamily: "sans-serif" }}
-      >
-        <div className={`footer-content border-top mt-5 border-${props.mode==="Dark"?"light":"dark"}`}>
+      <footer className={`text-center`} style={{ fontFamily: "sans-serif" }}>
+        <div
+          className={`footer-content border-top mt-5 border-${
+            props.mode === "Dark" ? "light" : "dark"
+          }`}
+        >
           <div className="d-flex">
-            <span className={`copyright text-${props.mode==="Dark"?"light":"dark"}`}>
-              &copy; 2023 nitinkumar2905.vercel.app  &nbsp; &nbsp; All right reserved
+            <span
+              className={`copyright text-${
+                props.mode === "Dark" ? "light" : "dark"
+              }`}
+            >
+              &copy; 2023 &nbsp;
+              <span className="text-decoration-underline link-offset-1" style={{cursor:'pointer'}} to="https://nitinkumar2905.vercel.app">
+                nitinkumar2905
+              </span>
+              &nbsp; &nbsp; All right reserved
             </span>
           </div>
         </div>

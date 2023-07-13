794409aedbe937c74fd466ab1cafdf6b081fbc93
Nitin kumar <nimble2905@gmail.com>
2023-07-13 13:10:13 +0530

    save project functionality added

--- src/Components/Routes/ProjectSection.jsx
+++ src/Components/Routes/ProjectSection.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import item1 from "../images/search.gif";
 import "../styles/Projects.css";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
+import { toast } from "react-hot-toast";
 
 const ProjectSection = (props) => {
+  const navigate = useNavigate();
   const [projects, setProjects] = useState([]);
   const [loading, setLoading] = useState(false);
-  const host = "https://nitinkumar-in-api.vercel.app";
-  // const host = "http://localhost:5000";
+  // const host = "https://nitinkumar-in-api.vercel.app";
+  const host = "http://localhost:8000";
+  const token = localStorage.getItem("token");
 
   const updateProjects = async () => {
     // setFetching(true);
     props.setProgress(10);
-    let url = `${host}/api/v1/projects`;
+    let url = `${host}/api/project/fetchAllProjects`;
     setLoading(true);
     let data = await fetch(url);
     props.setProgress(30);
@@ -31,6 +33,60 @@ const ProjectSection = (props) => {
     updateProjects();
     // eslint-disable-next-line
   }, []);
+
+  // to save a project user's profile's saved project if logged in
+
+  const saveProject = async (projectId) => {
+    if (!localStorage.getItem("token")) {
+      navigate("/login");
+      toast("Please login to continue", "warning", {
+        style: {
+          borderRadius: "10px",
+          background: `${props.mode === "Dark" ? "#fff" : "#333"}`,
+          color: `${props.mode === "Dark" ? "#333" : "#fff"}`,
+        },
+      });
+    } else {
+      try {
+        const response = await fetch(`${host}/api/project/save/${projectId}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": token,
+          },
+          body: JSON.stringify({ projectId }),
+        });
+
+        if (response.ok) {
+          toast("Project saved successfully", "success", {
+            style: {
+              borderRadius: "10px",
+              background: `${props.mode === "Dark" ? "#fff" : "#333"}`,
+              color: `${props.mode === "Dark" ? "#333" : "#fff"}`,
+            },
+          });
+        } else {
+          toast("Failed to save project", "error", {
+            style: {
+              borderRadius: "10px",
+              background: `${props.mode === "Dark" ? "#fff" : "#333"}`,
+              color: `${props.mode === "Dark" ? "#333" : "#fff"}`,
+            },
+          });
+        }
+      } catch (error) {
+        console.error(error);
+        toast("An error occurred while saving the project", "error", {
+          style: {
+            borderRadius: "10px",
+            background: `${props.mode === "Dark" ? "#fff" : "#333"}`,
+            color: `${props.mode === "Dark" ? "#333" : "#fff"}`,
+          },
+        });
+      }
+    }
+  };
+
   return (
     <>
       <div
@@ -51,14 +107,16 @@ const ProjectSection = (props) => {
               projects?.map((element) => {
                 const key = uuidv4();
                 return (
-                  <div className="" key={key}>
+                  <div className="project-map" key={key}>
                     <div
                       className={`my-2 project-item ${
                         props.mode === "Light" ? "itemLight" : "itemDark"
                       } rounded`}
                     >
-
-                      <div style={{minHeight:'40vh'}} className="my-2 d-flex flex-column align-items-start mx-3">
+                      <div
+                        style={{ minHeight: "40vh" }}
+                        className="my-2 d-flex flex-column align-items-start mx-3"
+                      >
                         <h4
                           className={`text-start text-decoration-underline underline-link-${
                             props.mode === "Dark" ? "light" : "dark"
@@ -77,7 +135,8 @@ const ProjectSection = (props) => {
                         </p>
                         <div>
                           <Link
-                            to="/contact"
+                            to={`${element.gitHub_Url}`}
+                            target="_blank"
                             className={`my-2 me-2 btn btn-outline-${
                               props.mode === "Light" ? "dark" : "light"
                             }`}
@@ -85,163 +144,20 @@ const ProjectSection = (props) => {
                             GitHub
                           </Link>
 
-                          <Link
-                            to="/projects"
+                          <button
+                            onClick={() => saveProject(element.projectId)}
                             className={`my-2 ms-2 btn btn-outline-${
                               props.mode === "Light" ? "dark" : "light"
                             }`}
                           >
-                            Save Projects 🖤
-                          </Link>
+                            Save Project
+                          </button>
                         </div>
                       </div>
                     </div>
                   </div>
                 );
               })}
-            {/* item number 1 */}
-
-            {/* <div
-              className={`my-2 project-item ${
-                props.mode === "Light" ? "itemLight" : "itemDark"
-              } rounded`}
-            >
-
-              <div className="my-2 d-flex flex-column align-items-start mx-3">
-                <h4
-                  className={`text-start text-decoration-underline underline-link-${
-                    props.mode === "Dark" ? "light" : "dark"
-                  } link-offset-2 my-3 mx-auto w-100 text-${
-                    props.mode === "Light" ? "dark" : "light"
-                  }`}
-                >
-                  NewsMonkey
-                </h4>
-                <p
-                  className={`text-start text-${
-                    props.mode === "Light" ? "dark" : "light"
-                  }`}
-                >
-                  NewsMonkey is a webapp application build using newsAPI and
-                  react framework for providing the daily updates.
-                </p>
-                <div>
-                  <Link
-                    to="/contact"
-                    className={`my-2 me-2 btn btn-outline-${
-                      props.mode === "Light" ? "dark" : "light"
-                    }`}
-                  >
-                    GitHub
-                  </Link>
-
-                  <Link
-                    to="/projects"
-                    className={`my-2 ms-2 btn btn-outline-${
-                      props.mode === "Light" ? "dark" : "light"
-                    }`}
-                  >
-                    Save Projects 🖤
-                  </Link>
-                </div>
-              </div>
-            </div> */}
-            {/* item number 1 */}
-            {/* <div
-              className={`my-2 project-item ${
-                props.mode === "Light" ? "itemLight" : "itemDark"
-              } rounded`}
-            >
-             
-              <div className="my-2 d-flex flex-column align-items-start mx-3">
-                <h4
-                  className={`text-start text-decoration-underline underline-link-${
-                    props.mode === "Dark" ? "light" : "dark"
-                  } link-offset-2 my-3 mx-auto w-100 text-${
-                    props.mode === "Light" ? "dark" : "light"
-                  }`}
-                >
-                  INotes
-                </h4>
-                <p
-                  className={`text-start text-${
-                    props.mode === "Light" ? "dark" : "light"
-                  }`}
-                >
-                  INotes is webapp application build using MERN stack tools
-                  which helps users to store daily notes on cloud within secure
-                  network and can access from anywhere.
-                </p>
-                <div>
-                  <Link
-                    to="/contact"
-                    className={`my-2 me-2 btn btn-outline-${
-                      props.mode === "Light" ? "dark" : "light"
-                    }`}
-                  >
-                    GitHub
-                  </Link>
-
-                  <Link
-                    to="/projects"
-                    className={`my-2 ms-2 btn btn-outline-${
-                      props.mode === "Light" ? "dark" : "light"
-                    }`}
-                  >
-                    Save Projects 🖤
-                  </Link>
-                </div>
-              </div>
-            </div> */}
-            {/* item number 1 */}
-            {/* <div
-              className={`my-2 project-item ${
-                props.mode === "Light" ? "itemLight" : "itemDark"
-              } rounded`}
-            >
-              <div className="my-2 d-flex flex-column align-items-start mx-3">
-                <h4
-                  className={`text-start text-decoration-underline underline-link-${
-                    props.mode === "Dark" ? "light" : "dark"
-                  } link-offset-2 my-3 mx-auto w-100 text-${
-                    props.mode === "Light" ? "dark" : "light"
-                  }`}
-                >
-                  VisionVault
-                </h4>
-                <p
-                  className={`text-start text-${
-                    props.mode === "Light" ? "dark" : "light"
-                  }`}
-                >
-                  VisionVault is a multimedia content sharing and storage
-                  platform. It provides users with a platform to upload,
-                  organize, and share various types of visual content, such as
-                  images, artwork, photographs, videos, and more. Users can
-                  create collections, curate their content, and connect with
-                  others who share similar interests.
-                </p>
-                <div>
-                  <Link
-                    to="/contact"
-                    className={`my-2 me-2 btn btn-outline-${
-                      props.mode === "Light" ? "dark" : "light"
-                    }`}
-                  >
-                    GitHub
-                  </Link>
-
-                  <Link
-                    to="/projects"
-                    className={`my-2 ms-2 btn btn-outline-${
-                      props.mode === "Light" ? "dark" : "light"
-                    }`}
-                  >
-                    Save Projects 🖤
-                  </Link>
-                </div>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
